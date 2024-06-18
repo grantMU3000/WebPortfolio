fb769777dc80c708fb294ee15e3b925b6601714d
robin329 <robin329@miamioh.edu>
2024-06-18 17:06:00 -0400

    Finished making the navbar. Made some changes to adapt it for mobile users

---
+++ app.js
@@ -0,0 +1,8 @@
+// Essentially going to be used for the animations
+    const menu = document.querySelector('#mobile-menu')
+    const menuLinks = document.querySelector('.navbar__menu')
+
+    menu.addEventListener('click', function() {
+        menu.classList.toggle('is-active');
+        menuLinks.classList.toggle('active');
+    });

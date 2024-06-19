36d07b29b33322178bd1a27314b9d097d8d9a384
robin329 <robin329@miamioh.edu>
2024-06-18 21:53:26 -0400

    Added some comments and made changes to my LinkedIn link so that it goes to my profile

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 // Essentially going to be used for the animations
-    const menu = document.querySelector('#mobile-menu')
-    const menuLinks = document.querySelector('.navbar__menu')
+    const menu = document.querySelector('#mobile-menu')  // Targets the mobile menu
+    const menuLinks = document.querySelector('.navbar__menu')  // targets the navbar menu
 
+    // Basically calls two functions in the CSS when the three lines are clicked
     menu.addEventListener('click', function() {
         menu.classList.toggle('is-active');
         menuLinks.classList.toggle('active');
